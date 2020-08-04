655a83124f7f0357b143d9ea767eb47ea57a5a51
Claudio Pereira <claudio_leal@hotmail.com>
2020-08-04 02:28:43 +0100

    posgres test

--- index.js
+++ index.js
@@ -8,21 +8,15 @@ app.use(bodyParser.json())
 const port = process.env.PORT || 4000;
 const dummyDb = { subscription: null } //dummy in memory store
 
+webpush.setVapidDetails(
+  'mailto:myuserid@email.com',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+)
 
-const pgOptions = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-};
-console.log(process.env.DATABASE_URL);
-//const { Pool } = require('pg');
-//const pool = new Pool(pgOptions);
 const Sequelize = require('sequelize')
-//const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
 const sequelize = new Sequelize(process.env.DATABASE_URL);
-sequelize
-.authenticate()
+sequelize.authenticate()
 .then(() => {
 	console.log('Connection has been established successfully.');
 })
@@ -38,6 +32,18 @@ const User = sequelize.define('user', {
 	lastName: {
 		type: Sequelize.STRING
 		// allowNull defaults to true
+	}
+}, {
+	// options
+});
+const Subscription = sequelize.define('subscription', {
+	// attributes
+	user: {
+		type: Sequelize.STRING,
+		//allowNull: false
+	},
+	raw: {
+		type: Sequelize.STRING
 	}
 }, {
 	// options
@@ -81,49 +87,10 @@ const saveToDatabase = async subscription => {
   //dummyDb.subscription = subscription
 }
 
-console.log('server');
-const query = async (qu, callback) => {
-
-/*
-	const { Client } = require('pg')
-	const client = new Client(pgOptions)
-	await client.connect()
-	const res = await client.query(qu)
-	await client.end()
-return res;
-*/
-	const { Client } = require('pg');
-	const db = new Client(pgOptions);
-	db.connect();
-
-	db.query(qu, (err, qres) => {
-		if (err)
-			throw err;
-		callback(qres);
-		console.log(JSON.stringify(qres.rows));
-		db.end();
-	});
-}
-
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1> ' + JSON.stringify(dummyDb)))
 
 app.get('/sub', async (req, res) => {
-	//query('select * from subscriptions', (r)=>{ res.body = JSON.stringify(r.rows) });	
-/*
-	pool
-  .query('select * from subscriptions')
-  .then(r => {
-	  res.json = r.rows;
-	  console.log('subscriptions:', r.rows);
-  })
-  .catch(err =>
-    setImmediate(() => {
-      throw err
-    })
-  )
-*/
 });
-
 
 // The new /save-subscription endpoint
 app.post('/save-subscription', async (req, res) => {
@@ -132,17 +99,10 @@ app.post('/save-subscription', async (req, res) => {
   res.json({ message: 'success' })
 })
 
-const vapidKeys = {
-  publicKey:
-    'BOTPzboE4C_uWvQyJfZb2wmGiZ353PfPPmiaht_krkseJMAoAOxnH-2ohIyC1om_bgUoNgNyqK6Q6ICk1KmgnI8',
-  privateKey: 'JoYZJO-0akJLwdWB6-D1sbJ5rzbejsCWQ6bXEHRgdhA',
-}
-//setting our previously generated VAPID keys
-webpush.setVapidDetails(
-  'mailto:myuserid@email.com',
-  vapidKeys.publicKey,
-  vapidKeys.privateKey
-)
+//const vapidKeys = {
+//  publicKey:	process.env.VAPID_PUBLIC_KEY, 		// e.g. 'BOTPzboE4C_uWvQyJfZb2wmGiZ353PfPPmiaht_krkseJMAoAOxnH-2ohIyC1om_bgUoNgNyqK6Q6ICk1KmgnI8',
+//  privateKey:	process.env.VAPID_PRIVATE_KEY		// e.g. 'JoYZJO-0akJLwdWB6-D1sbJ5rzbejsCWQ6bXEHRgdhA',
+//}
 
 //function to send the notification to the subscribed device
 const sendNotification = (subscription, dataToSend) => {
@@ -157,66 +117,3 @@ app.get('/send-notification', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-
-
-
-
-/*
-const express = require('express')
-const Sequelize = require('sequelize')
-const app = express()
-const port = 3000
-app.use(express.json());
-const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
-sequelize
-.authenticate()
-.then(() => {
-console.log('Connection has been established successfully.');
-})
-.catch(err => {
-console.error('Unable to connect to the database:', err);
-});
-const User = sequelize.define('user', {
-// attributes
-firstName: {
-type: Sequelize.STRING,
-allowNull: false
-},
-lastName: {
-type: Sequelize.STRING
-// allowNull defaults to true
-}
-}, {
-// options
-});
-// Note: using `force: true` will drop the table if it already exists
-User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
-app.get('/', (req, res) => res.json({ message: 'Hello World' }))
-app.post('/user', async (req, res) => {
-try {
-const newUser = new User(req.body)
-await newUser.save()
-res.json({ user: newUser }) // Returns the new user that is created in the database
-} catch(error) {
-console.error(error)
-}
-})
-app.get('/user/:userId', async (req, res) => {
-const userId = req.params.userId
-try {
-const user = await User.findAll({
-where: {
-id: userId
-}
-}
-)
-res.json({ user })
-} catch(error) {
-console.error(error)
-}
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-*/
